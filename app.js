bd6cd472f611b9611757da5c2ba83942107559de
Jeremy Lu <jeremy@pubulous.com>
2015-03-30 15:53:13 +0800

    feature(view): support co-views

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 var render = require('co-render');
+var views = require('co-views');
 var koa = require('koa');
 var co = require('co');
 var router = require('koa-router')();
@@ -20,19 +21,21 @@ router.get( '/', function *(){
 
 	// opts: 
 	// isNonStatic: 是否打上 data-react-id
-	// 差別就在 (isNonStatic) ? engine.renderToString(parsed) : engine.renderToStaticMarkup(parsed);
+	// 		- 差別就在 (isNonStatic) ? engine.renderToString(parsed) : engine.renderToStaticMarkup(parsed);
 	// enableCache: true|false
 	// base: html base file
-	var resultString = yield render( 'templates/user.jsx', 
-									 { 
-										// 這個是真正傳入 react template 的資料
-										user: loki, 
-
-										// 下面這三個是給 template engine 看的
-										engine: 'react', 
-										tags: 'content', 
-										base:'templates/index.html' 
-									 });
+	// tags: html 內 {{content}} 的變數名稱
+	
+	// 將常用參數預先傳入，得回一支 render() 指令，方便之後用
+	var render = views( 'templates', 
+						{ default: 'jsx', 
+						  map:{ jsx: 'react'}, 
+						  locals: {tags: 'content', base:'templates/index.html'}});
+	
+	// 之後使用時只要指定需要的 tempalte file 名稱，就會去 'templates/user.jsx' 內找檔案
+	var resultString = yield render( 'user', 
+									 // 這個是真正傳入 react template 的資料
+									 { user: loki });
 
 	// 示範如要用傳統 jade, handlebar 等模板的語法
 	// var b = render( 'templates/user.jade', { user: loki, engine: 'jade' });
