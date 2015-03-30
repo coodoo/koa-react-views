26de5ab5d116104810fe26fd96f142bd6cbdd4f5
Jeremy Lu <jeremy@pubulous.com>
2015-03-30 16:33:44 +0800

    feat(view): complete support for co-views, will not use koa-views

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
+var co = require('co');
+var koa = require('koa');
 var render = require('co-render');
 var views = require('co-views');
-var koa = require('koa');
-var co = require('co');
 var router = require('koa-router')();
 
 var app = koa();
@@ -19,18 +19,30 @@ var loki = {
 // router 只處理 GET / 的請求
 router.get( '/', function *(){
 
-	// opts: 
-	// isNonStatic: 是否打上 data-react-id
-	// 		- 差別就在 (isNonStatic) ? engine.renderToString(parsed) : engine.renderToStaticMarkup(parsed);
-	// enableCache: true|false
-	// base: html base file
-	// tags: html 內 {{content}} 的變數名稱
+	// co-views  的好處是將 template engine 必用參數預先傳入，
+	// 得回一支 render() 指令，方便之後用
 	
-	// 將常用參數預先傳入，得回一支 render() 指令，方便之後用
+	// 'templates' 是放 jsx 檔案的位置
 	var render = views( 'templates', 
-						{ default: 'jsx', 
-						  map:{ jsx: 'react'}, 
-						  locals: {tags: 'content', base:'templates/index.html'}});
+						
+						{ 
+							// 預設副檔名，將來 render() 時就不用重寫
+							default: 'jsx', 
+
+							// 指定 jsx 副檔名要使用 react template engine 來處理
+							map:{ jsx: 'react'}, 
+
+							// 這是最終要傳入 consolidate 內的參數，參數有: 
+							// isNonStatic: 是否打上 data-reactid
+							// 	 - 差別就在 (isNonStatic) ? engine.renderToString(parsed) : engine.renderToStaticMarkup(parsed);
+							// enableCache: true|false
+							// base: html base file
+							// tags: html 內 {{content}} 的變數名稱
+							locals: {
+								isNonStatic: false,
+								tags: 'content', 
+								base:'templates/index.html'}
+						});
 	
 	// 之後使用時只要指定需要的 tempalte file 名稱，就會去 'templates/user.jsx' 內找檔案
 	var resultString = yield render( 'user', 
